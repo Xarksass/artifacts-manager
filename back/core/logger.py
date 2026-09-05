fff5601cb56ccebe352cc1c6eaa62e18443255dd
Xarksass <disalvo.infographiste@gmail.com>
2026-09-05 21:02:59 +0200

    logger(backend): formatter updated for the console handler to use the uvicorn default formatter

--- back/core/logger.py
+++ back/core/logger.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from dotenv import load_dotenv
+from uvicorn.logging import DefaultFormatter
 
 load_dotenv()
 
@@ -17,21 +18,28 @@ levels = {
 def get_logger(name: str, file: str, level: str|None = None) -> logging.Logger:
     if level is None or level not in levels:
         level = os.getenv('LOG_LEVEL','warning')
+
     logger = logging.getLogger(name)
+
     file_handler = logging.FileHandler(f"logs/{file}.log", mode="a", encoding="utf-8")
     console_handler = logging.StreamHandler()
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+
     file_formatter = logging.Formatter(
     "{asctime} - {levelname} - {message}",
         style="{",
         datefmt="%Y-%m-%d %H:%M",
     )
-    console_formatter = logging.Formatter(
-    "{levelname} - {message}",
-        style="{"
+
+    # Formatter console : celui d'uvicorn, avec le même pattern que "uvicorn.error"
+    console_formatter = DefaultFormatter(
+        fmt="%(levelprefix)s %(message)s",
+        use_colors=True,  # None = auto-détection du tty, True = forcé
     )
+
     file_handler.setFormatter(file_formatter)
     console_handler.setFormatter(console_formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
     logger.setLevel(levels[level])
     return logger
